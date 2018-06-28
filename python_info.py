5a3d34f88430a746a53baed5a3c93841acfaa5ce
Ross Wilson <rzzzwilson@gmail.com>
2018-06-28 15:44:53 +0700

    Remove unused files

--- python_info.py
+++
@@ -1,9 +0,0 @@
-"""
-Little program to get python version and platform info.
-
-Designed to write results to "stdout".
-"""
-
-import platform
-
-print('Python %s on %s' % (platform.python_version().strip(), platform.platform().strip()))
